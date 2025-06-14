90ffab27f439f52ddb0f7bd08a822f9712c9da3b
aliviahossain <aliviahossainofficial@gmail.com>
2025-06-14 17:44:54 +0530

    Enhance UI: improve layout, buttons, fonts, responsiveness, and colors

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    host: true,
-    port: 5173,
+  server: {
+    host: true,       // allows access from network devices (not just localhost)
+    port: 5173,       // default Vite port, change if needed
     watch: {
-      usePolling: true,
+      usePolling: true, // helpful for environments like Docker or WSL where file watching can be problematic
     },
   },
 })
